d8d53ecb23fc022f808974c600f1df39e1d1a42b
tpatkai <tpatkai@gmail.com>
2015-03-08 22:02:24 +0000

    added and set up devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
 before_action :find_post, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, except: [:index, :show]
     
     def index
       @posts= Post.all.order("created_at DESC")
     end
     
     def new
-      @post= Post.new
+      @post= current_user.posts.build
     end
     
     def show
@@ -14,7 +15,7 @@ before_action :find_post, only: [:show, :edit, :update, :destroy]
     end
     
     def create
-      @post= Post.new(post_params)
+      @post= current_user.posts.build(post_params)
       
       if @post.save
         redirect_to @post
