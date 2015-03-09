dc164ea21ad048b0b9bac78897e441246d16d054
tpatkai <tpatkai@gmail.com>
2015-03-09 11:26:56 +0000

    comments work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   has_attached_file :image, styles:  { :medium => "600x400#>", :small => "300x200>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
