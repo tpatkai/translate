4a6c0c1ede46ef2600d916d9c4e42f7a9c185d96
tpatkai <tpatkai@gmail.com>
2015-04-22 21:02:35 +0000

    added styling, random posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 before_action :find_post, only: [:show, :edit, :update, :destroy]
-before_action :authenticate_user!, except: [:index, :show]
+before_action :authenticate_user!, except: [:index, :show, :create]
 		
 		def index
 			if params[:category].blank?#
@@ -20,6 +20,7 @@ before_action :authenticate_user!, except: [:index, :show]
 		
 		def show
 			@comments = Comment.where(post_id: @post)
+			@random_post= Post.where.not(id: @post).order("RANDOM()").first
 		end
 		
 		def create
